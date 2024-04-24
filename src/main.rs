0a1f9b8d9095b0b20f3118a9429d8abb5e7f80c0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2024-04-23 20:06:01 -0600

    feat: add handler

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,46 @@
+use axum::routing::get;
+
+use socketioxide::{
+    extract::{Data, SocketRef, State},
+    SocketIo,
+};
+use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+use tower::ServiceBuilder;
+use tower_http::cors::CorsLayer;
+
+#[derive(Debug, serde::Deserialize)]
+struct MessageIn {
+    room: String,
+    text: String,
+}
+
+#[derive(serde::Serialize)]
+struct Messages {
+    messages: Vec<state::Message>,
+}
+
+async fn handler(axum::extract::State(io): axum::extract::State<SocketIo>) {
+    info!("handler called");
+    let _ = io.emit("hello", "world");
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
+
+    let app = axum::Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/hello", get(handler))
+        .with_state(io)
+        .layer(
+            ServiceBuilder::new()
+                .layer(CorsLayer::permissive())
+                .layer(layer),
+        );
+
+    info!("Starting server");
+
+
     Ok(())
 }
